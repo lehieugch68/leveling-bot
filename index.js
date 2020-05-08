b5b3187ee95ddccb8c77a315ab79a161162c2f45
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-05-08 18:18:25 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const TOKEN = ''; //token bot
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./Discord.db'); //tệp SQLite
+
+const options = {
+  timeOut: 60,
+  xpmin: 10,
+  xpmax: 20,
+  lvlupXP: 500
+}
+
+const LevelSystem = require('./levelSystem.js');
+const levelSystem = new LevelSystem(client, db);
+
+client.login(TOKEN)
