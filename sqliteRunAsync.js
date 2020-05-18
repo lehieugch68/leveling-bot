0ac5598f8b6544e2270733fe847f9d0f58271aa3
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-05-19 01:27:50 +0700

    Update sqliteRunAsync.js

--- sqliteRunAsync.js
+++ sqliteRunAsync.js
@@ -1,8 +1,8 @@
 function sqliteRunAsync (db, query) {
 	return new Promise(async (resolve, reject) => {
 		db.run(query, (err) => {
-			if (err) reject(err);
-			resolve();
+			if (err) return reject(err);
+			return resolve();
 		})
 	})
 }
