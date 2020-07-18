7757a135f278cec54f15c14114223468e99bad01
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-07-18 22:56:14 +0700

    Update levelSystem.js

--- levelSystem.js
+++ levelSystem.js
@@ -1,4 +1,5 @@
 const sqliteRunAsync = require('./sqliteRunAsync.js')
+const events = require('events');
 
 class Talker {
 	constructor(guildID) {
@@ -8,47 +9,43 @@ class Talker {
 }
 
 class LevelSystem {
-	constructor(client, db, options) {
-		this.client = client;
+	constructor(db, options = {cooldown: 60, xpmin: 10, xpmax: 20, lvlupXp: 500}) {
+		this.event = new events.EventEmitter();
 		this.db = db;
 		this.talkedRecently = [];
-		this.cooldown = options.cooldown||60;
-		this.xpmin = options.xpmin||10;
-		this.xpmax = options.xpmax||20;
-		this.lvlupXp = options.lvlupXp||500;
-		client.on('message', async msg => {
-			if (msg.author.bot) return undefined;
+		this.cooldown = options.cooldown;
+		this.xpmin = options.xpmin;
+		this.xpmax = options.xpmax;
+		this.lvlupXp = options.lvlupXp;
+		this.event.on('message', async msg => {
 			if (!this.talkedRecently.find(g => g.guildID === msg.guild.id)) {
 				let talkerGuild = new Talker(msg.guild.id);
 				this.talkedRecently.push(talkerGuild);
 			}
 			let talker = this.talkedRecently.find(g => g.guildID === msg.guild.id).talkerID;
-			if (talker.every(id => id !== msg.author.id)) {
-				this.talkedRecently.find(g => g.guildID === msg.guild.id).talkerID.push(msg.author.id);
-				var xp = randomXP(this.xpmin, this.xpmax);
-				var point = xp, level = 0;
-				try {
-					await sqliteRunAsync(this.db, `CREATE TABLE IF NOT EXISTS '${msg.guild.id}' (id VARCHAR(30) PRIMARY KEY, point INTEGER NULL, level INTEGER NULL)`);
-					this.db.get(`SELECT * FROM '${msg.guild.id}' WHERE id = '${msg.author.id}'`, (err, row) => {
-						if (err) return console.log(err);
-						if (row) {
-							point += row.point;
-							level = (point >= this.lvlupXp) ? Math.floor(point/this.lvlupXp) : 0;
-						}
-						this.db.run(`INSERT INTO '${msg.guild.id}' (id, point, level) VALUES ('${msg.author.id}', ${point}, ${level}) ON CONFLICT(id) DO UPDATE SET point = ${point}, level = ${level}`);
-					});
-				} catch (error) {
-					return console.log(error);
+			if (talker.some(id => id === msg.author.id)) return;
+			let xp = randomXP(this.xpmin, this.xpmax);
+			let point = xp, level = 0;
+			try {
+				await sqlite3Async.Run(this.db, `CREATE TABLE IF NOT EXISTS '${msg.guild.id}' (id VARCHAR(30) PRIMARY KEY, point INTEGER NULL, level INTEGER NULL)`);
+				let row = await sqlite3Async.Get(this.db, `SELECT * FROM '${msg.guild.id}' WHERE id = '${msg.author.id}'`);
+				if (row != null) {
+					point += row.point;
+					level = (point >= this.lvlupXp) ? Math.floor(point/this.lvlupXp) : 0;
 				}
-				setTimeout(() => {
-					this.talkedRecently.find(g => g.guildID === msg.guild.id).talkerID = this.talkedRecently.find(g => g.guildID === msg.guild.id).talkerID.filter(id => id !== msg.author.id);
-				}, 1000*this.cooldown)
+				await sqlite3Async.Run(this.db, `INSERT INTO '${msg.guild.id}' (id, point, level) VALUES ('${msg.author.id}', ${point}, ${level}) ON CONFLICT(id) DO UPDATE SET point = ${point}, level = ${level}`);
+			} catch (err) { 
+				return console.log(err);
 			}
+			this.talkedRecently.find(g => g.guildID === msg.guild.id).talkerID.push(msg.author.id);
+			setTimeout(() => {
+				this.talkedRecently.find(g => g.guildID === msg.guild.id).talkerID = this.talkedRecently.find(g => g.guildID === msg.guild.id).talkerID.filter(id => id !== msg.author.id);
+			}, 1000*this.cooldown);
 		});
 	}
 }
 
-function randomXP(xpmin, xpmax) {
+let randomXP = (xpmin, xpmax) => {
 	return Math.floor(Math.random() * (xpmax - xpmin + 1)) + xpmin;
 }
 
